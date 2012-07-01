1c9b2d1daf04bb2eddc9150fb27ca3b22048feb4
Heinz N. Gies <heinz@licenser.net>
2012-07-01 23:02:26 +0200

    Fixed bugs in cpu chart.

--- apps/wiggle/priv/js/cpu_charts.js
+++ apps/wiggle/priv/js/cpu_charts.js
@@ -51,15 +51,15 @@ var mpstat = new Object();
 		.enter()
 		.append("user")
 		.attr("x", function(d, i) { return obj.x(i) - .5; })
-		.attr("y", function(d) { return obj.h - obj.y(d.values[0].user) - .5; })
+		.attr("y", function(d) { return obj.h - obj.y(d.values[i].user) - .5; })
 		.attr("width", obj.w)
-		.attr("height", function(d) { return obj.y(d.values[0].user); })
+		.attr("height", function(d) { return obj.y(d.values[i].user); })
 		.attr("class", "user")
 		.append("system")
 		.attr("x", function(d, i) { return obj.x(i) - .5; })
-		.attr("y", function(d) { return obj.h - obj.y(d.values[0].system) - .5 - obj.y(d.values[0].user); })
+		.attr("y", function(d) { return obj.h - obj.y(d.values[i].system) - .5 - obj.y(d.values[i].user); })
 		.attr("width", obj.w)
-		.attr("height", function(d) { return obj.y(d.values[0].system); })
+		.attr("height", function(d) { return obj.y(d.values[i].system); })
 		.attr("class", "system");
 	    
 	    obj.chart[i].append("line")
@@ -84,44 +84,44 @@ var mpstat = new Object();
 	    
 		rect_user.enter().insert("rect", "line")
 		    .attr("x", function(d, i) { return obj.x(i + 1) - .5; })
-		    .attr("y", function(d) { return obj.h - obj.y(d.values[0].user) - .5; })
+		    .attr("y", function(d) { return obj.h - obj.y(d.values[i].user) - .5; })
 		    .attr("width", obj.w)
-		    .attr("height", function(d) { return obj.y(d.values[0].user); })
+		    .attr("height", function(d) { return obj.y(d.values[i].user); })
 		    .attr("class", "user")
 		    .transition()
-		    .duration(800)
+		    .duration(0)
 		    .attr("x", function(d, i) { return obj.x(i) - .5; });
 		rect_system.enter().insert("rect", "line")
 		    .attr("x", function(d, i) { return obj.x(i + 1) - .5; })
-		    .attr("y", function(d) { return obj.h - obj.y(d.values[0].system) - .5 - obj.y(d.values[0].user); })
+		    .attr("y", function(d) { return obj.h - obj.y(d.values[i].system) - .5 - obj.y(d.values[i].user); })
 		    .attr("width", obj.w)
-		    .attr("height", function(d) { return obj.y(d.values[0].system); })
+		    .attr("height", function(d) { return obj.y(d.values[i].system); })
 		    .attr("class", "system")
 		    .transition()
-		    .duration(800)
+		    .duration(0)
 		    .attr("x", function(d, i) { return obj.x(i) - .5; });
 		
 		rect_user.transition()
-		    .duration(800)
+		    .duration(0)
 		    .attr("color", "red")
 		    .attr("x", function(d, i) { return obj.x(i) - .5; })
-		    .attr("y", function(d) { return obj.h - obj.y(d.values[0].user) - .5; })
-		    .attr("height", function(d) { return obj.y(d.values[0].user); })
+		    .attr("y", function(d) { return obj.h - obj.y(d.values[i].user) - .5; })
+		    .attr("height", function(d) { return obj.y(d.values[i].user); })
 		    .attr("class", "user");
 		rect_system.transition()
-		    .duration(800)
+		    .duration(0)
 		    .attr("x", function(d, i) { return obj.x(i) - .5; })
-		    .attr("y", function(d) { return obj.h - obj.y(d.values[0].system) - .5 - obj.y(d.values[0].user); })
-		    .attr("height", function(d) { return obj.y(d.values[0].system); })
+		    .attr("y", function(d) { return obj.h - obj.y(d.values[i].system) - .5 - obj.y(d.values[i].user); })
+		    .attr("height", function(d) { return obj.y(d.values[i].system); })
 		    .attr("class", "system");
 		
 		rect_user.exit().transition()
-		    .duration(800)
+		    .duration(0)
 		    .attr("x", function(d, i) { return obj.x(i - 1) - .5; })
 		    .remove();
 		
 		rect_system.exit().transition()
-		    .duration(800)
+		    .duration(0)
 		    .attr("x", function(d, i) { return obj.x(i - 1) - .5; })
 		    .remove();
 	    }
