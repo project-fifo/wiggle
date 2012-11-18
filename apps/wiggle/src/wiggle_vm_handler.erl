68079c6b34b5777551dc96c17a24f79e8daf056e
Heinz N. Gies <heinz@licenser.net>
2012-11-19 00:40:21 +0100

    Added vm methods.

--- apps/wiggle/src/wiggle_vm_handler.erl
+++ apps/wiggle/src/wiggle_vm_handler.erl
@@ -5,12 +5,15 @@
 
 -export([init/3, 
 	 rest_init/2]).
+
 -export([content_types_provided/2, 
 	 content_types_accepted/2,
 	 allowed_methods/2,
-	 delete_resource/2,
 	 resource_exists/2,
+	 delete_resource/2,
 	 forbidden/2,
+	 post_is_create/2,
+	 create_path/2,
 	 is_authorized/2]).
 -export([to_json/2,
 	 from_json/2]).
@@ -18,11 +21,11 @@
 -record(state, {path, method, version, token, content, reply}).
  
 init(_Transport, _Req, []) ->
-	{upgrade, protocol, cowboy_http_rest}.
+    {upgrade, protocol, cowboy_http_rest}.
 
 rest_init(Req, _) ->
     {Method, Req1} = cowboy_http_req:method(Req),
-    {[<<"api">>, Version, <<"groups">> | Path], Req2} = cowboy_http_req:path(Req1),
+    {[<<"api">>, Version, <<"vms">> | Path], Req2} = cowboy_http_req:path(Req1),
     {Token, Req3} = case cowboy_http_req:header(<<"X-Snarl-Token">>, Req2) of
 			{undefined, ReqX} -> 
 			    {undefined, ReqX};
@@ -46,44 +49,28 @@ content_types_accepted(Req, State) ->
       {<<"application/json; charset=UTF-8">>, from_json}
      ], Req, State}.
 
+post_is_create(Req, State) ->
+    {true, Req, State}.
+
 allowed_methods(Req, State) ->
     {['HEAD' | allowed_methods(State#state.version, State#state.token, State#state.path)], Req, State}.
 
 allowed_methods(_Version, _Token, []) ->
-    ['GET'];
+    ['GET', 'POST'];
 
-allowed_methods(_Version, _Token, [_Group]) ->
-    ['GET', 'PUT', 'DELETE'];
-
-allowed_methods(_Version, _Token, [_Group, <<"permissions">>]) ->
-    ['GET'];
-
-allowed_methods(_Version, _Token, [_Group, <<"permissions">> | _Permission]) ->
-    ['PUT', 'DELETE'].
-
-resource_exists(Req, State = #state{path = [Group, <<"permissions">> | Permission]}) ->
-    case {erlangify_permission(Permission), libsnarl:group_get(Group)} of
-	{_, {reply, {ok, not_found}}} ->
-	    {false, Req, State};
-	{[], {reply, {ok, _}}} ->
-	    {true, Req, State};
-	{P, {reply, {ok, {group, _Name, Permissions, _}}}} ->
-	    {lists:member(P, Permissions), Req, State}
-    end;
+allowed_methods(_Version, _Token, [_Vm]) ->
+    ['GET', 'PUT', 'DELETE'].
 
 resource_exists(Req, State = #state{path = []}) ->
     {true, Req, State};
 
-resource_exists(Req, State = #state{path = [Group | _]}) ->
-    case libsnarl:group_get(Group) of
+resource_exists(Req, State = #state{path = [Vm]}) ->
+    case libsniffle:vm_get(Vm) of
 	{reply, {ok, not_found}} ->
 	    {false, Req, State};
-	{reply, {ok, _}} ->
+	{reply, _} ->
 	    {true, Req, State}
     end.
-
-is_authorized(Req, State = #state{path = [_, <<"sessions">>]}) -> 
-    {true, Req, State};
 
 is_authorized(Req, State = #state{token = undefined}) -> 
     {{false, <<"X-Snarl-Token">>}, Req, State};
@@ -91,36 +78,23 @@ is_authorized(Req, State = #state{token = undefined}) ->
 is_authorized(Req, State) -> 
     {true, Req, State}.
 
-forbidden(Req, State = #state{path = [_, <<"sessions">>]}) ->
-    {false, Req, State};
-
 forbidden(Req, State = #state{token = undefined}) -> 
     {true, Req, State};
 
-forbidden(Req, State = #state{path = []}) ->
-    {allowed(State#state.token, [<<"groups">>]), Req, State};
+forbidden(Req, State = #state{method = 'GET', path = []}) ->
+    {allowed(State#state.token, [<<"vms">>]), Req, State};
 
-forbidden(Req, State = #state{method = 'GET', path = [Group]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"get">>]), Req, State};
+forbidden(Req, State = #state{method = 'POST', path = []}) ->
+    {allowed(State#state.token, [<<"vms">>, <<"create">>]), Req, State};
 
-forbidden(Req, State = #state{method = 'PUT', path = [Group]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"create">>]), Req, State};
+forbidden(Req, State = #state{method = 'GET', path = [Vm]}) ->
+    {allowed(State#state.token, [<<"vms">>, Vm, <<"get">>]), Req, State};
 
-forbidden(Req, State = #state{method = 'DELETE', path = [Group]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"delete">>]), Req, State};
+forbidden(Req, State = #state{method = 'DELETE', path = [Vm]}) ->
+    {allowed(State#state.token, [<<"vms">>, Vm, <<"delete">>]), Req, State};
 
-forbidden(Req, State = #state{method = 'GET', path = [Group, <<"permissions">>]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"get">>]), Req, State};
-
-forbidden(Req, State = #state{method = 'PUT', path = [Group, <<"permissions">> | Permission]}) ->
-    P = erlangify_permission(Permission),
-    {allowed(State#state.token, [<<"groups">>, Group, <<"grant">>])
-     andalso allowed(State#state.token, [<<"permissions">>, P, <<"grant">>]), Req, State};
-
-forbidden(Req, State = #state{method = 'DELETE', path = [Group, <<"permissions">> | Permission]}) ->
-    P = erlangify_permission(Permission),
-    {allowed(State#state.token, [<<"groups">>, Group, <<"revoke">>])
-     andalso allowed(State#state.token, [<<"permissions">>, P, <<"revoke">>]), Req, State};
+forbidden(Req, State = #state{method = 'PUT', path = [Vm]}) ->
+    {allowed(State#state.token, [<<"vms">>, Vm, <<"edit">>]), Req, State};
 
 forbidden(Req, State) ->
     {true, Req, State}.
@@ -134,21 +108,36 @@ to_json(Req, State) ->
     {jsx:encode(Reply), Req1, State1}.
 
 handle_request(Req, State = #state{path = []}) ->
-    {reply, {ok, Res}} = libsnarl:group_list(),
+    {reply, {ok, Res}} = libsniffle:vm_list(),
     {Res, Req, State};
 
-handle_request(Req, State = #state{path = [Group]}) ->
-    {reply, {ok, {group, Name, Permissions, _}}} = libsnarl:group_get(Group),
-    {[{name, Name},
-      {permissions, Permissions}], Req, State};
+handle_request(Req, State = #state{path = [Vm]}) ->
+    {reply, {ok, {vm, Name, _, Hypervisor, Dict}}} = libsniffle:vm_get(Vm),
+    {[{<<"uuid">>, Name}, 
+      {<<"hypervisor">>, Hypervisor} | dict:to_list(Dict)], Req, State}.
 
-handle_request(Req, State = #state{path = [Group, <<"permissions">>]}) ->
-    {reply, {ok, {group, _Name, Permissions, _}}} = libsnarl:group_get(Group),
-    {Permissions, Req, State}.
 
 %%--------------------------------------------------------------------
 %% PUT
 %%--------------------------------------------------------------------
+
+
+create_path(Req, State = #state{path = [], version = Version, token = Token}) ->
+    {ok, Body, Req1} = cowboy_http_req:body(Req),
+    {Decoded, Req2} = case Body of
+			  <<>> ->
+			      {[], Req1};
+			  _ ->
+			      D = jsx:decode(Body),
+			      {D, Req1}
+		      end,
+    {<<"dataset">>, Dataset} = lists:keyfind(<<"dataset">>, 1, Decoded),
+    {<<"package">>, Package} = lists:keyfind(<<"package">>, 1, Decoded),
+    {<<"config">>, Config} = lists:keyfind(<<"config">>, 1, Decoded),
+    {reply, {ok, {user, Owner, _, _, _, _}}} = libsnarl:user_get({token, Token}),
+    {reply, {ok, UUID}} = libsniffle:create(Package, Dataset, [{<<"owner">>, Owner} | Config]),
+    {<<"/api/", Version/binary, "/vms/", UUID/binary>>, Req2, State}.
+
 
 from_json(Req, State) ->
     {ok, Body, Req1} = cowboy_http_req:body(Req),
@@ -161,39 +150,31 @@ from_json(Req, State) ->
 			    end,
     {Reply, Req2, State1}.
 
-handle_write(Req, State = #state{path = [Group]}, []) ->
-    libsnarl:group_add(Group),
+handle_write(Req, State = #state{path = [Vm]}, [{<<"action">>, <<"start">>}]) ->
+    libsniffle:vm_start(Vm),
     {true, Req, State};
 
-handle_write(Req, State = #state{path = [Group, <<"permissions">> | Permission]}, []) ->
-    P = erlangify_permission(Permission),
-    {reply, ok} = libsnarl:group_grant(Group, P),
-    {true, Req, State}.
+handle_write(Req, State = #state{path = [Vm]}, [{<<"action">>, <<"stop">>}]) ->
+    libsniffle:vm_stop(Vm),
+    {true, Req, State};
 
+handle_write(Req, State = #state{path = [Vm]}, [{<<"action">>, <<"reboot">>}]) ->
+    libsniffle:vm_reboot(Vm),
+    {true, Req, State};
+
+handle_write(Req, State = #state{path = []}, _Body) ->
+    {true, Req, State};
+
+handle_write(Req, State, _Body) ->
+    {fase, Req, State}.
 
 %%--------------------------------------------------------------------
 %% DEETE
 %%--------------------------------------------------------------------
 
-delete_resource(Req, State = #state{path = [Group, <<"permissions">> | Permission]}) ->
-    P = erlangify_permission(Permission),
-    {reply, ok} = libsnarl:group_revoke(Group, P),
-    {true, Req, State};
-
-delete_resource(Req, State = #state{path = [Group]}) ->
-    {reply, ok} = libsnarl:group_delete(Group),
+delete_resource(Req, State = #state{path = [Vm]}) ->
+    {reply, ok} = libsniffle:vm_delete(Vm),
     {true, Req, State}.
-
-%% Internal Functions
-
-erlangify_permission(P) ->
-    lists:map(fun(<<"...">>) ->
-		      '...';
-		 (<<"_">>) ->
-		      '_';
-		 (E) ->
-		      E
-	      end, P).
 
 allowed(Token, Perm) ->
     case libsnarl:allowed({token, Token}, Perm) of
