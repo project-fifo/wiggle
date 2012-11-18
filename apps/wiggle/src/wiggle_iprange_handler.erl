a9b01d0057ddd029f6a6378f1ff2750ef8422fb3
Heinz N. Gies <heinz@licenser.net>
2012-11-18 21:36:08 +0100

    Implemented ipranges.

--- apps/wiggle/src/wiggle_iprange_handler.erl
+++ apps/wiggle/src/wiggle_iprange_handler.erl
@@ -9,8 +9,8 @@
 -export([content_types_provided/2, 
 	 content_types_accepted/2,
 	 allowed_methods/2,
-	 delete_resource/2,
 	 resource_exists/2,
+	 delete_resource/2,
 	 forbidden/2,
 	 is_authorized/2]).
 -export([to_json/2,
@@ -19,11 +19,11 @@
 -record(state, {path, method, version, token, content, reply}).
  
 init(_Transport, _Req, []) ->
-	{upgrade, protocol, cowboy_http_rest}.
+    {upgrade, protocol, cowboy_http_rest}.
 
 rest_init(Req, _) ->
     {Method, Req1} = cowboy_http_req:method(Req),
-    {[<<"api">>, Version, <<"groups">> | Path], Req2} = cowboy_http_req:path(Req1),
+    {[<<"api">>, Version, <<"ipranges">> | Path], Req2} = cowboy_http_req:path(Req1),
     {Token, Req3} = case cowboy_http_req:header(<<"X-Snarl-Token">>, Req2) of
 			{undefined, ReqX} -> 
 			    {undefined, ReqX};
@@ -53,38 +53,19 @@ allowed_methods(Req, State) ->
 allowed_methods(_Version, _Token, []) ->
     ['GET'];
 
-allowed_methods(_Version, _Token, [_Group]) ->
-    ['GET', 'PUT', 'DELETE'];
-
-allowed_methods(_Version, _Token, [_Group, <<"permissions">>]) ->
-    ['GET'];
-
-allowed_methods(_Version, _Token, [_Group, <<"permissions">> | _Permission]) ->
-    ['PUT', 'DELETE'].
-
-resource_exists(Req, State = #state{path = [Group, <<"permissions">> | Permission]}) ->
-    case {erlangify_permission(Permission), libsnarl:group_get(Group)} of
-	{_, {reply, {ok, not_found}}} ->
-	    {false, Req, State};
-	{[], {reply, {ok, _}}} ->
-	    {true, Req, State};
-	{P, {reply, {ok, {group, _Name, Permissions, _}}}} ->
-	    {lists:member(P, Permissions), Req, State}
-    end;
+allowed_methods(_Version, _Token, [_Iprange]) ->
+    ['GET', 'PUT', 'DELETE'].
 
 resource_exists(Req, State = #state{path = []}) ->
     {true, Req, State};
 
-resource_exists(Req, State = #state{path = [Group | _]}) ->
-    case libsnarl:group_get(Group) of
+resource_exists(Req, State = #state{path = [Iprange]}) ->
+    case libsniffle:iprange_get(Iprange) of
 	{reply, {ok, not_found}} ->
 	    {false, Req, State};
-	{reply, {ok, _}} ->
+	{reply, _} ->
 	    {true, Req, State}
     end.
-
-is_authorized(Req, State = #state{path = [_, <<"sessions">>]}) -> 
-    {true, Req, State};
 
 is_authorized(Req, State = #state{token = undefined}) -> 
     {{false, <<"X-Snarl-Token">>}, Req, State};
@@ -92,36 +73,21 @@ is_authorized(Req, State = #state{token = undefined}) ->
 is_authorized(Req, State) -> 
     {true, Req, State}.
 
-forbidden(Req, State = #state{path = [_, <<"sessions">>]}) ->
-    {false, Req, State};
 
 forbidden(Req, State = #state{token = undefined}) -> 
     {true, Req, State};
 
 forbidden(Req, State = #state{path = []}) ->
-    {allowed(State#state.token, [<<"groups">>]), Req, State};
+    {allowed(State#state.token, [<<"ipranges">>]), Req, State};
 
-forbidden(Req, State = #state{method = 'GET', path = [Group]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"get">>]), Req, State};
+forbidden(Req, State = #state{method = 'GET', path = [Iprange]}) ->
+    {allowed(State#state.token, [<<"ipranges">>, Iprange, <<"get">>]), Req, State};
 
-forbidden(Req, State = #state{method = 'PUT', path = [Group]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"create">>]), Req, State};
+forbidden(Req, State = #state{method = 'DELETE', path = [Iprange]}) ->
+    {allowed(State#state.token, [<<"ipranges">>, Iprange, <<"delete">>]), Req, State};
 
-forbidden(Req, State = #state{method = 'DELETE', path = [Group]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"delete">>]), Req, State};
-
-forbidden(Req, State = #state{method = 'GET', path = [Group, <<"permissions">>]}) ->
-    {allowed(State#state.token, [<<"groups">>, Group, <<"get">>]), Req, State};
-
-forbidden(Req, State = #state{method = 'PUT', path = [Group, <<"permissions">> | Permission]}) ->
-    P = erlangify_permission(Permission),
-    {allowed(State#state.token, [<<"groups">>, Group, <<"grant">>])
-     andalso allowed(State#state.token, [<<"permissions">>, P, <<"grant">>]), Req, State};
-
-forbidden(Req, State = #state{method = 'DELETE', path = [Group, <<"permissions">> | Permission]}) ->
-    P = erlangify_permission(Permission),
-    {allowed(State#state.token, [<<"groups">>, Group, <<"revoke">>])
-     andalso allowed(State#state.token, [<<"permissions">>, P, <<"revoke">>]), Req, State};
+forbidden(Req, State = #state{method = 'PUT', path = [Iprange]}) ->
+    {allowed(State#state.token, [<<"ipranges">>, Iprange, <<"edit">>]), Req, State};
 
 forbidden(Req, State) ->
     {true, Req, State}.
@@ -135,17 +101,31 @@ to_json(Req, State) ->
     {jsx:encode(Reply), Req1, State1}.
 
 handle_request(Req, State = #state{path = []}) ->
-    {reply, {ok, Res}} = libsnarl:group_list(),
+    {reply, {ok, Res}} = libsniffle:iprange_list(),
     {Res, Req, State};
 
-handle_request(Req, State = #state{path = [Group]}) ->
-    {reply, {ok, {group, Name, Permissions, _}}} = libsnarl:group_get(Group),
-    {[{name, Name},
-      {permissions, Permissions}], Req, State};
+handle_request(Req, State = #state{path = [Iprange]}) ->
+    {reply, {ok, {iprange, 
+		  Name, 
+		  Network,
+		  Gateway,
+		  Netmask,
+		  First,
+		  Last,
+		  Current,
+		  _,
+		  Free}}} = libsniffle:iprange_get(Iprange),
+    {[
+      {name, Name},
+      {network, ip_to_str(Network)},
+      {gateway, ip_to_str(Gateway)},
+      {netmask, ip_to_str(Netmask)},
+      {first, ip_to_str(First)},
+      {last, ip_to_str(Last)},
+      {current, ip_to_str(Current)},
+      {free, lists:map(fun(IP) -> ip_to_str(IP) end, Free)}
+     ], Req, State}.
 
-handle_request(Req, State = #state{path = [Group, <<"permissions">>]}) ->
-    {reply, {ok, {group, _Name, Permissions, _}}} = libsnarl:group_get(Group),
-    {Permissions, Req, State}.
 
 %%--------------------------------------------------------------------
 %% PUT
@@ -162,39 +142,26 @@ from_json(Req, State) ->
 			    end,
     {Reply, Req2, State1}.
 
-handle_write(Req, State = #state{path = [Group]}, []) ->
-    libsnarl:group_add(Group),
+handle_write(Req, State = #state{path = [Iprange]}, Body) ->
+    {<<"network">>, Network} = lists:keyfind(<<"network">>, 1, Body),
+    {<<"gateway">>, Gateway} = lists:keyfind(<<"gateway">>, 1, Body),
+    {<<"netmask">>, Netmask} = lists:keyfind(<<"netmask">>, 1, Body),
+    {<<"first">>, First} = lists:keyfind(<<"first">>, 1, Body),
+    {<<"last">>, Last} = lists:keyfind(<<"last">>, 1, Body),
+						% We use the name as tag.
+    {reply, ok} = libsniffle:iprange_create(Iprange, Network, Gateway, Netmask, First, Last, Iprange),
     {true, Req, State};
 
-handle_write(Req, State = #state{path = [Group, <<"permissions">> | Permission]}, []) ->
-    P = erlangify_permission(Permission),
-    {reply, ok} = libsnarl:group_grant(Group, P),
-    {true, Req, State}.
-
+handle_write(Req, State, _Body) ->
+    {fase, Req, State}.
 
 %%--------------------------------------------------------------------
 %% DEETE
 %%--------------------------------------------------------------------
 
-delete_resource(Req, State = #state{path = [Group, <<"permissions">> | Permission]}) ->
-    P = erlangify_permission(Permission),
-    {reply, ok} = libsnarl:group_revoke(Group, P),
-    {true, Req, State};
-
-delete_resource(Req, State = #state{path = [Group]}) ->
-    {reply, ok} = libsnarl:group_delete(Group),
+delete_resource(Req, State = #state{path = [Iprange]}) ->
+    {reply, ok} = libsniffle:iprange_delete(Iprange),
     {true, Req, State}.
-
-%% Internal Functions
-
-erlangify_permission(P) ->
-    lists:map(fun(<<"...">>) ->
-		      '...';
-		 (<<"_">>) ->
-		      '_';
-		 (E) ->
-		      E
-	      end, P).
 
 allowed(Token, Perm) ->
     case libsnarl:allowed({token, Token}, Perm) of
@@ -205,3 +172,8 @@ allowed(Token, Perm) ->
 	{reply, false} ->
 	    true
     end.
+
+
+ip_to_str(Ip) ->
+    <<A:8, B:8, C:8, D:8>> = <<Ip:32>>,
+    list_to_binary(io_lib:format("~p.~p.~p.~p", [A, B, C, D])).
