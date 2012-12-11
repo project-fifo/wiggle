b31a4dcedd8a9b89022db5a5ba1123df39725e21
Heinz N. Gies <heinz@licenser.net>
2012-12-12 00:46:27 +0100

    Fixed install script.

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -26,11 +26,12 @@ case $2 in
 	chown -R wiggle:wiggle /var/log/wiggle
 	;;
     POST-INSTALL)
-	if svcs svc:/network/wiggle:default1 > /dev/null 2&>1
+	if svcs svc:/network/wiggle:default > /dev/null 2>&1
+	then
+	    echo Service already existings ...
+	else
 	    echo Importing service ...
 	    svccfg import /opt/local/wiggle/etc/wiggle.xml
-	else
-	    echo Service already existings ...
 	fi
 	echo Trying to guess configuration ...
 	IP=`ifconfig net0 | grep inet | awk -e '{print $2}'`
