35eb66f414fd9d4675071950f762db3f378737f1
Heinz N. Gies <heinz@licenser.net>
2012-12-28 05:25:12 +0100

    Improved install script for ssl certificate gernation.

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -56,7 +56,7 @@ case $2 in
             openssl req \
                 -new \
                 -batch \
-                -subj "$(echo -n "$subj" | /opt/local/gnu/bin/tr "\n" "/")" \
+                -subj "$(/opt/local/gnu/bin/echo -n "$subj" | /opt/local/gnu/bin/tr "\n" "/")" \
                 -key $CERTDIR/$DOMAIN.key \
                 -out $CERTDIR/$DOMAIN.csr \
                 -passin env:PASSPHRASE
