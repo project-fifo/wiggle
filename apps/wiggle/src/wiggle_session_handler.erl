7386e3f60525bc454861c5bf9a450ac00dd55b4e
Heinz N. Gies <heinz@licenser.net>
2013-01-12 02:47:28 +0100

    Fixed problem in session handler.

--- apps/wiggle/src/wiggle_session_handler.erl
+++ apps/wiggle/src/wiggle_session_handler.erl
@@ -45,7 +45,32 @@ init(_Transport, _Req, []) ->
     {upgrade, protocol, cowboy_http_rest}.
 
 rest_init(Req, _) ->
-    wiggle_handler:initial_state(Req, <<"sessions">>).
+    {Method, Req1} = cowboy_http_req:method(Req),
+    {[<<"api">>, Version, <<"sniffle">> | Path], Req2} = cowboy_http_req:path(Req1),
+    {ok, Req3} = cowboy_http_req:set_resp_header(<<"Access-Control-Allow-Origin">>, <<"*">>, Req2),
+    {Token, Req4} = case cowboy_http_req:header(<<"X-Snarl-Token">>, Req3) of
+                        {undefined, ReqX} ->
+                            {TokenX, ReqX1} = cowboy_http_req:cookie(<<"X-Snarl-Token">>, ReqX),
+                            {TokenX, ReqX1};
+                        {TokenX, ReqX} ->
+                            {ok, ReqX1} = cowboy_http_req:set_resp_header(<<"X-Snarl-Token">>, TokenX, ReqX),
+                            {TokenX, ReqX1}
+                    end,
+    {ok, Req5} = cowboy_http_req:set_resp_header(
+                   <<"Access-Control-Allow-Headers">>,
+                   <<"Content-Type, X-Snarl-Token">>, Req4),
+    {ok, Req6} = cowboy_http_req:set_resp_header(
+                   <<"Access-Control-Expose-Headers">>,
+                   <<"X-Snarl-Token">>, Req5),
+    {ok, Req7} = cowboy_http_req:set_resp_header(
+                   <<"Allow-Access-Control-Credentials">>,
+                   <<"true">>, Req6),
+    State =  #state{version = Version,
+                    method = Method,
+                    token = Token,
+                    path = Path},
+    io:format("[~p] - ~p~n", [Method, Path]),
+    {ok, Req7, State}.
 
 post_is_create(Req, State) ->
     {true, Req, State}.
