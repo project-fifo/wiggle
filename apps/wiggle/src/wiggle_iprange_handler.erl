47458d16b91887c73dc334b1aa99e94f4f6acf70
Heinz N. Gies <heinz@licenser.net>
2013-04-28 18:16:52 +0200

    Added metrics for mypervisors and groups.

--- apps/wiggle/src/wiggle_iprange_handler.erl
+++ apps/wiggle/src/wiggle_iprange_handler.erl
@@ -18,7 +18,8 @@
          options/2,
          create_path/2,
          post_is_create/2,
-         is_authorized/2]).
+         is_authorized/2,
+         rest_terminate/2]).
 
 -export([to_json/2,
          from_json/2,
@@ -41,7 +42,8 @@
               create_path/2,
               post_is_create/2,
               resource_exists/2,
-              rest_init/2]).
+              rest_init/2,
+              rest_terminate/2]).
 
 init(_Transport, _Req, []) ->
     {upgrade, protocol, cowboy_rest}.
@@ -49,33 +51,22 @@ init(_Transport, _Req, []) ->
 rest_init(Req, _) ->
     wiggle_handler:initial_state(Req).
 
+rest_terminate(_Req, State) ->
+    ?M(?P(State), State#state.start),
+    ok.
+
 post_is_create(Req, State) ->
     {true, Req, State}.
 
 service_available(Req, State) ->
-    case {libsniffle:servers(), libsnarl:servers()} of
-        {[], _} ->
-            {false, Req, State};
-        {_, []} ->
-            {false, Req, State};
-        _ ->
-            {true, Req, State}
-    end.
+    {wiggle_handler:service_available(), Req, State}.
 
 options(Req, State) ->
     Methods = allowed_methods(State#state.version, State#state.token, State#state.path),
-    Req1 = cowboy_req:set_resp_header(
-             <<"access-control-allow-methods">>,
-             string:join(
-               lists:map(fun erlang:binary_to_list/1,
-                         [<<"HEAD">>, <<"OPTIONS">> | Methods]), ", "), Req),
-    {ok, Req1, State}.
+    wiggle_handler:options(Req, State,Methods).
 
 content_types_provided(Req, State) ->
-    {[
-      {<<"application/json">>, to_json},
-      {<<"application/x-msgpack">>, to_msgpack}
-     ], Req, State}.
+    {wiggle_handler:provided(), Req, State}.
 
 content_types_accepted(Req, State) ->
     {wiggle_handler:accepted(), Req, State}.
@@ -96,10 +87,13 @@ resource_exists(Req, State = #state{path = []}) ->
     {true, Req, State};
 
 resource_exists(Req, State = #state{path = [Iprange | _]}) ->
+    Start = now(),
     case libsniffle:iprange_get(Iprange) of
         not_found ->
+            ?MSniffle(?P(State), Start),
             {false, Req, State};
         {ok, Obj} ->
+            ?MSniffle(?P(State), Start),
             {true, Req, State#state{obj = Obj}}
     end.
 
@@ -119,25 +113,25 @@ forbidden(Req, State = #state{token = undefined}) ->
     {true, Req, State};
 
 forbidden(Req, State = #state{method = <<"GET">>, path = []}) ->
-    {allowed(State#state.token, [<<"cloud">>, <<"ipranges">>, <<"list">>]), Req, State};
+    {wiggle_handler:allowed(State, [<<"cloud">>, <<"ipranges">>, <<"list">>]), Req, State};
 
 forbidden(Req, State = #state{method = <<"POST">>, path = []}) ->
-    {allowed(State#state.token, [<<"cloud">>, <<"ipranges">>, <<"create">>]), Req, State};
+    {wiggle_handler:allowed(State, [<<"cloud">>, <<"ipranges">>, <<"create">>]), Req, State};
 
 forbidden(Req, State = #state{method = <<"GET">>, path = [Iprange]}) ->
-    {allowed(State#state.token, [<<"ipranges">>, Iprange, <<"get">>]), Req, State};
+    {wiggle_handler:allowed(State, [<<"ipranges">>, Iprange, <<"get">>]), Req, State};
 
 forbidden(Req, State = #state{method = <<"DELETE">>, path = [Iprange]}) ->
-    {allowed(State#state.token, [<<"ipranges">>, Iprange, <<"delete">>]), Req, State};
+    {wiggle_handler:allowed(State, [<<"ipranges">>, Iprange, <<"delete">>]), Req, State};
 
 forbidden(Req, State = #state{method = <<"PUT">>, path = [_Iprange]}) ->
-    {allowed(State#state.token, [<<"cloud">>, <<"ipranges">>, <<"create">>]), Req, State};
+    {wiggle_handler:allowed(State, [<<"cloud">>, <<"ipranges">>, <<"create">>]), Req, State};
 
 forbidden(Req, State = #state{method = <<"PUT">>, path = [Iprange, <<"metadata">> | _]}) ->
-    {allowed(State#state.token, [<<"ipranges">>, Iprange, <<"edit">>]), Req, State};
+    {wiggle_handler:allowed(State, [<<"ipranges">>, Iprange, <<"edit">>]), Req, State};
 
 forbidden(Req, State = #state{method = <<"DELETE">>, path = [Iprange, <<"metadata">> | _]}) ->
-    {allowed(State#state.token, [<<"ipranges">>, Iprange, <<"edit">>]), Req, State};
+    {wiggle_handler:allowed(State, [<<"ipranges">>, Iprange, <<"edit">>]), Req, State};
 
 forbidden(Req, State) ->
     {true, Req, State}.
@@ -185,10 +179,13 @@ create_path(Req, State = #state{path = [], version = Version}) ->
     {ok, Last} = jsxd:get(<<"last">>, Data),
     {ok, Tag} = jsxd:get(<<"tag">>, Data),
     Vlan = jsxd:get(<<"vlan">>, 0, Data),
+    Start = now(),
     case libsniffle:iprange_create(Iprange, Network, Gateway, Netmask, First, Last, Tag, Vlan) of
         {ok, UUID} ->
+            ?MSniffle(?P(State), Start),
             {<<"/api/", Version/binary, "/ipranges/", UUID/binary>>, Req1, State#state{body = Data}};
         duplicate ->
+            ?MSniffle(?P(State), Start),
             {ok, Req2} = cowboy_req:reply(409, Req1),
             {halt, Req2, State}
     end.
@@ -222,7 +219,9 @@ handle_write(Req, State = #state{method = <<"POST">>, path = []}, _) ->
     {true, Req, State};
 
 handle_write(Req, State = #state{path = [Iprange, <<"metadata">> | Path]}, [{K, V}]) ->
+    Start = now(),
     libsniffle:iprange_set(Iprange, [<<"metadata">> | Path] ++ [K], jsxd:from_list(V)),
+    ?MSniffle(?P(State), Start),
     {true, Req, State};
 
 handle_write(Req, State, _Body) ->
@@ -233,23 +232,16 @@ handle_write(Req, State, _Body) ->
 %%--------------------------------------------------------------------
 
 delete_resource(Req, State = #state{path = [Iprange, <<"metadata">> | Path]}) ->
+    Start = now(),
     libsniffle:iprange_set(Iprange, [<<"metadata">> | Path], delete),
+    ?MSniffle(?P(State), Start),
     {true, Req, State};
 
 delete_resource(Req, State = #state{path = [Iprange]}) ->
+    Start = now(),
     ok = libsniffle:iprange_delete(Iprange),
+    ?MSniffle(?P(State), Start),
     {true, Req, State}.
-
-allowed(Token, Perm) ->
-    case libsnarl:allowed({token, Token}, Perm) of
-        not_found ->
-            true;
-        true ->
-            false;
-        false ->
-            true
-    end.
-
 
 ip_to_str(Ip) ->
     <<A:8, B:8, C:8, D:8>> = <<Ip:32>>,
