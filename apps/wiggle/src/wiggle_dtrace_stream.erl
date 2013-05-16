e8fb88544f87217d09421bbc429075791aab4d5f
Heinz N. Gies <heinz@licenser.net>
2013-05-16 06:01:45 +0200

    Merged dtrace fix.

--- apps/wiggle/src/wiggle_dtrace_stream.erl
+++ apps/wiggle/src/wiggle_dtrace_stream.erl
@@ -28,7 +28,7 @@ websocket_init(_Any, Req, []) ->
     {ID, Req1} = cowboy_req:binding(uuid, Req0),
     Req2 = wiggle_handler:set_access_header(Req1),
     {Encoder, Decoder, Type} = case C of
-                                   <<"msgpack">> ->
+                                   [<<"msgpack">> | _] ->
                                        {fun(O) ->
                                                 msgpack:pack(O, [jsx])
                                         end,
@@ -37,7 +37,7 @@ websocket_init(_Any, Req, []) ->
                                                 jsxd:from_list(O)
                                         end,
                                         binary};
-                                   <<"json">> ->
+                                   [<<"json">> | _ ] ->
                                        {fun(O) ->
                                                 jsx:encode(O)
                                         end,
@@ -47,7 +47,7 @@ websocket_init(_Any, Req, []) ->
                                end,
     case wiggle_handler:get_token(Req2) of
         {undefined, Req3} ->
-            e(401, Req3);
+            e(401, <<".">>, Req3);
         {Token, Req3} ->
             case libsnarl:allowed({token, Token}, [<<"dtrace">>, ID, <<"stream">>]) of
                 true ->
@@ -59,7 +59,7 @@ websocket_init(_Any, Req, []) ->
                             e(404, Req3)
                     end;
                 false ->
-                    e(401, Req3)
+                    e(403, <<"forbidden">>, Req3)
             end
     end.
 
@@ -123,7 +123,6 @@ handle(Config, Req, State  = #state{encoder = Enc, type = Type}) ->
     Config2 = jsxd:update([<<"servers">>], fun(S) ->
                                                    S
                                            end, Servers, Config1),
-    io:format("Now we got a config: ~p~n", [Config2]),
     case libsniffle:dtrace_run(State#state.id, Config2) of
         {ok, S} ->
             {reply, {Type, Enc(jsxd:merge(Config1, State#state.config))},
