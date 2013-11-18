342dadc378d99640f5227b82951f280a2e301ae9
Heinz N. Gies <heinz@licenser.net>
2013-11-18 16:41:44 +0100

    Added vm change owner endpoint.

--- apps/wiggle/src/wiggle_package_handler.erl
+++ apps/wiggle/src/wiggle_package_handler.erl
@@ -81,7 +81,7 @@ read(Req, State = #state{path = [_Package], obj = Obj}) ->
 
 create(Req, State = #state{path = [], version = Version}, Data) ->
     Data1 = jsxd:select([<<"cpu_cap">>, <<"quota">>, <<"ram">>,
-                         <<"requirements">>, <<"zfs_io_priority">>], Data),
+                         <<"requirements">>, <<"zfs_iox_priority">>], Data),
     {ok, Package} = jsxd:get(<<"name">>, Data),
     case libsniffle:package_create(Package) of
         {ok, UUID} ->
