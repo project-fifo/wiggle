b7f74e7b9f8d5276bd7615e2542bf69590ad8a87
Heinz N. Gies <heinz@licenser.net>
2014-05-09 09:07:12 +0200

    Fixed bug in package creation that prevented zfs_io_priority to work.

--- apps/wiggle/src/wiggle_package_handler.erl
+++ apps/wiggle/src/wiggle_package_handler.erl
@@ -93,7 +93,7 @@ read(Req, State = #state{path = [_Package], obj = Obj}) ->
 
 create(Req, State = #state{path = [], version = Version}, Data) ->
     Data1 = jsxd:select([<<"cpu_cap">>, <<"quota">>, <<"ram">>,
-                         <<"requirements">>, <<"zfs_iox_priority">>,
+                         <<"requirements">>, <<"zfs_io_priority">>,
                          <<"max_swap">>], Data),
     {ok, Package} = jsxd:get(<<"name">>, Data),
     case libsniffle:package_create(Package) of
