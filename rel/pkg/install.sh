69713ee0e6770df7539a1fa5cb760ac23e815876
Heinz N. Gies <heinz@licenser.net>
2014-12-09 13:23:28 +0100

    added config update

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -41,16 +41,10 @@ case $2 in
         then
             cp ${CONFFILE}.example ${CONFFILE}
             sed --in-place -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
-            md5sum ${CONFFILE} > ${CONFFILE}.md5
-        elif [ -f ${CONFFILE}.md5 ]
-        then
-            if md5sum --quiet --strict -c ${CONFFILE}.md5 2&> /dev/null
-            then
-                echo "The config was not adjusted we'll regenerate it."
-                cp ${CONFFILE}.example ${CONFFILE}
-                sed --in-place -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
-                md5sum ${CONFFILE} > ${CONFFILE}.md5
-            fi
+        else
+            /opt/local/fifo-wiggle/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
+                mv ${CONFFILE} ${CONFFILE}.old &&
+                mv ${CONFFILE}.new ${CONFFILE}
         fi
         ;;
 esac
